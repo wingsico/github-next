67e5e7b82af55adf601164e16b0a5e026670a718
wingsico <wingsico@gmail.com>
2019-05-06 17:54:53 +0800

    [update]: next config - add css loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-module.exports = {
+const withCSS = require('@zeit/next-css')
+/**
+ * next 官网推荐写法
+ */
+if (typeof require !== undefined) {
+  require.extensions['.css'] = file => ({})
+}
+
+/**
+ * 如果有一个叫 withLess
+ * 则可以 withLess(withCSS({}))
+ */
+
+module.exports = withCSS({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -7,4 +20,4 @@ module.exports = {
 
     return config
   }
-}
+})
