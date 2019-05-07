7ac2f89462c79a491ad883ce9a4970d592bed8bf
wingsico <wingsico@gmail.com>
2019-05-07 19:54:09 +0800

    [test]: test Router && Link && events

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,40 @@
 import { Button } from 'antd'
+import Link from 'next/link'
+import Router from 'next/router'
 
-export default () => <Button>Hello</Button>
+const events = [
+  'routeChangeStart',
+  'routeChangeComplete',
+  'beforeHistoryChange',
+  'routeChangeError',
+  'hashChangeStart',
+  'hashChangeComplete',
+]
+
+function makeEvent(type) {
+  return (...args) => {
+    console.log(type, ...args)
+  }
+}
+
+events.forEach(ev => {
+  Router.events.on(ev, makeEvent(ev));
+})
+
+export default () => {
+  function arriveToB() {
+    Router.push({
+      pathname: '/test/b',
+      query: {
+        id: 3,
+      }
+    }, '/test/b/3')
+  }
+  return (
+    <>
+      <Link href="/a?id=1" as="/a/1"><Button>A</Button></Link>
+      <Button onClick={arriveToB}>B</Button>
+      <Link href="/a#haha"><Button>Hash A</Button></Link>
+    </>
+  )
+}
