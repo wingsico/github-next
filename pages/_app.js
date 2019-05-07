898a7dc13f0295fc658f4f38bdd343f907fbd99e
wingsico <wingsico@gmail.com>
2019-05-07 19:53:12 +0800

    [test]: try the usage of custom app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 /**
  * 覆盖默认app.js
  */
-import App from 'next/app'
+import App, { Container } from 'next/app'
+import Layout from '../components/Layout'
 import 'antd/dist/antd.css'
 
-export default App;
+class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps }
+  }
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Container>
+
+    )
+  }
+}
+
+export default MyApp;
